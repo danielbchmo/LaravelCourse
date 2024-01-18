e951538508d518cb1729af220b78a2f64d26a5c5
danielbchmo <115262423+danielbchmo@users.noreply.github.com>
2024-01-18 10:30:58 -0600

    Adding form

--- sistema/resources/views/employee/create.blade.php
+++ sistema/resources/views/employee/create.blade.php
@@ -1,1 +1,19 @@
 Creation Employee Form
+<form action="" method="post" enctype="multipart/form-data">
+    <label for="nombre">Nombre </label>
+    <input type="text" name="nombre"> <br>
+
+    <label for="appPaterno">Apellido Paterno </label>
+    <input type="text" name="appPaterno"> <br>
+
+    <label for="appMaterno">Apellido Materno </label>
+    <input type="text" name="appMaterno"> <br>
+
+    <label for="correo">Correo </label>
+    <input type="email" name="correo"> <br>
+
+    <label for="foto">Foto </label>
+    <input type="file" name="foto"> <br>
+
+    <input type="submit" name="enviar">
+</form>
