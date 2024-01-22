c556e44386072afc1f22819886ba9048c1430a75
danielbchmo <115262423+danielbchmo@users.noreply.github.com>
2024-01-22 16:48:17 -0600

    Adding information by db

--- sistema/app/Http/Controllers/EmpleadoController.php
+++ sistema/app/Http/Controllers/EmpleadoController.php
@@ -12,7 +12,8 @@ class EmpleadoController extends Controller
      */
     public function index()
     {
-        return view('employee.index');
+        $data['empleados']=Empleado::paginate(5);
+        return view('empleado.index', $data);
     }
 
     /**
